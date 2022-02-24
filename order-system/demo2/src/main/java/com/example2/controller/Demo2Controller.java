7bdf6522cf2ce2b9cf5142a69152216877ff1015
Zep <18582036610@163.com>
2022-02-24 16:34:49 +0800

    2-24 add module demo2 & change module demo1 controller

--- order-system/demo2/src/main/java/com/example2/controller/Demo2Controller.java
+++ order-system/demo2/src/main/java/com/example2/controller/Demo2Controller.java
@@ -1,14 +1,14 @@
-package com.example1.controller;
+package com.example2.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class DemoController {
+public class Demo2Controller {
 
     @GetMapping("/")
     public String test1() {
-        return "hello world:8080";
+        return "hello world:8081";
     }
 
 }
