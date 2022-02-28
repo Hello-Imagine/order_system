ab62508d57ae6d6824a230bc3926ccec0ecc0081
Zep <18582036610@163.com>
2022-02-28 10:21:14 +0800

    2-28 10:21 add module menu & implement dynamic menu

---
+++ order-system/menu/src/main/java/com/menu/MenuApplication.java
@@ -0,0 +1,13 @@
+package com.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuApplication.class, args);
+    }
+
+}
